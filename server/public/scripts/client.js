a1e029d4e83b2bf0be6d84e6f5226699454cdbae
Neota Moe <neota.moe@gmail.com>
2017-05-11 17:01:04 -0500

    finished assignment

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,34 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('HighFiveController', function(){
+myApp.controller('HighFiveController', function(RandomNumber, CountSuccess ){
   console.log('angular');
   var vm=this;
-  vm.count=0;
+  // generate random number (skill level/quality of character on page load)
+  vm.agentQuality = RandomNumber.random();
+  console.log('agent:', vm.agentQuality);
+
+  // function that adds to current count
+  vm.counter = function(){
+    CountSuccess.setCount();
+    CountSuccess.getCount();
+  };
+
+  // vm.count = function(){
+  //   vm.success=CountSuccess.getCount();
+  //   return vm.success;
+  // };
+
+  // current count displaying on DOM at start
+
+
+  vm.checkQuality = function(){
+    vm.userQuality = RandomNumber.random();
+    console.log('userQuality:', vm.userQuality);
+    vm.success=CountSuccess.getCount();
+    if(vm.userQuality >= vm.agentQuality){
+      vm.counter();
+      vm.success=CountSuccess.getCount();
+    }
+  };  // end checkQuality
+
 }); // end controller
--- server/public/scripts/service.js
+++ server/public/scripts/service.js
@@ -1,3 +1,17 @@
-myApp.service('randomNumber', function(){
-  Math.floor((Math.random() * 10) + 1);
-});
+myApp.service('RandomNumber', function(){
+  this.random=function(){
+    return (Math.floor((Math.random() * 10) + 1));
+  };  // end random
+});  // end RandomNumber service
+
+myApp.service('CountSuccess', function(){
+  var count = 0;
+
+  this.getCount = function(){
+    return count;
+  };  // end getCount
+
+  this.setCount = function(){
+    count++;
+  };  // end setCount
+});  // end CountSuccess service
